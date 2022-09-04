961e0691f125499939b552d17d75779998f6972f
Danilo Santos <danilocarsan@gmail.com>
2022-09-04 00:24:47 -0300

    estilizando itens do grid

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -10,9 +10,12 @@ type Props = {
 
 const GridItem = ({ item, onClick }: Props) => {
     return (
-        <C.Container onClick={onClick}>
+        <C.Container
+            showBackground={item.shown || item.permanentShown}
+            onClick={onClick}
+        >
             {!item.permanentShown && !item.shown &&
-                <C.Icon src={b7svg} alt="b7web" />
+                <C.Icon src={b7svg} alt="b7web" opacity={0.2} />
             }
             {(item.shown || item.permanentShown) && item.item !== null &&
                 <C.Icon src={items[item.item].icon} alt={items[item.item].name} />
--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -1,9 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    background: black;
+type ContainerProps = {
+    showBackground: boolean;
+};
+
+export const Container = styled.div<ContainerProps>`
+    background: ${(props) => props.showBackground ? "#1550ff" : "#e2e3e3"};
+    height: 100%;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
 `;
 
-export const Icon = styled.img`
+type IconProps = {
+    opacity?: number;
+};
 
+export const Icon = styled.img<IconProps>`
+    width: 50px;
+    height: 50px;
+    opacity: ${(props) => props.opacity ?? 1};
 `;
